832a4a26fed6a653cab8d234839da39f7536efd4
FrozenDragon60Rus <naum6666@yandex.ru>
2024-02-04 00:27:32 +0300

    0.0.2

---
+++ TestWebApplication/Views/Shared/Introdution.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestWebApplication.Views.Shared
+{
+    public class IntrodutionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TestWebApplication/Views/Shared/SecondSection.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestWebApplication.Views.Shared
+{
+    public class SeconsSectionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
